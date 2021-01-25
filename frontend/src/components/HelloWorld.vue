1fcce9dd71611815f66d172249744319e56862ee
GXM <1641092084@qq.com>
2021-01-25 18:14:52 +0800

    save

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -4,6 +4,9 @@
   <p>
     Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
+  <img
+    src="//localhost:8012/images/article/6844904178075058189?url=https%3A%2F%2Fuser-gold-cdn.xitu.io%2F2020%2F6%2F1%2F1726f38e4afb7b1d%3FimageView2%2F0%2Fw%2F1280%2Fh%2F960%2Fformat%2Fwebp%2Fignore-error%2F1"
+  />
 </template>
 
 <script lang="ts">
